b08de4c6af0c68f4c2533202a9cce779f6110bd7
Baran Khan <bk@zapna.com>
2012-01-04 15:58:59 +0500

    test commit

--- lib/filter/base/BaseCustomerProductFormFilter.class.php
+++ lib/filter/base/BaseCustomerProductFormFilter.class.php
@@ -14,15 +14,17 @@ class BaseCustomerProductFormFilter extends BaseFormFilterPropel
   public function setup()
   {
     $this->setWidgets(array(
-      'customer_id' => new sfWidgetFormPropelChoice(array('model' => 'Customer', 'add_empty' => true)),
-      'product_id'  => new sfWidgetFormPropelChoice(array('model' => 'Product', 'add_empty' => true)),
-      'created_at'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'customer_id'       => new sfWidgetFormPropelChoice(array('model' => 'Customer', 'add_empty' => true)),
+      'product_id'        => new sfWidgetFormPropelChoice(array('model' => 'Product', 'add_empty' => true)),
+      'created_at'        => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'product_status_id' => new sfWidgetFormFilterInput(),
     ));
 
     $this->setValidators(array(
-      'customer_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Customer', 'column' => 'id')),
-      'product_id'  => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Product', 'column' => 'id')),
-      'created_at'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
+      'customer_id'       => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Customer', 'column' => 'id')),
+      'product_id'        => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Product', 'column' => 'id')),
+      'created_at'        => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
+      'product_status_id' => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
     ));
 
     $this->widgetSchema->setNameFormat('customer_product_filters[%s]');
@@ -40,10 +42,11 @@ class BaseCustomerProductFormFilter extends BaseFormFilterPropel
   public function getFields()
   {
     return array(
-      'id'          => 'Number',
-      'customer_id' => 'ForeignKey',
-      'product_id'  => 'ForeignKey',
-      'created_at'  => 'Date',
+      'id'                => 'Number',
+      'customer_id'       => 'ForeignKey',
+      'product_id'        => 'ForeignKey',
+      'created_at'        => 'Date',
+      'product_status_id' => 'Number',
     );
   }
 }
