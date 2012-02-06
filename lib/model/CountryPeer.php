da507ea49ee90c39ff314575f78ce8f3ce823b56
Khan Muhammad <khan.muhammad@zerocall.com>
2012-02-06 17:57:44 +0500

    redirect changes

--- lib/model/CountryPeer.php
+++ lib/model/CountryPeer.php
@@ -4,7 +4,7 @@ class CountryPeer extends BaseCountryPeer
 {
      static public function getSortedCountries() {
         $c = new Criteria();
-         $c->addAnd(CountryPeer::ID,182);
+         $c->addAnd(CountryPeer::ID,73);
         $c->addAscendingOrderByColumn(CountryPeer::NAME);
         $rs = CountryPeer::doSelect($c);
         return $rs;
