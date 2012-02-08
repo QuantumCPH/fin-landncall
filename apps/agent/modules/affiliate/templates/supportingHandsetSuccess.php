49daeac848387eef68b8f1915bce5f1bff2e30f8
Fatima Umer <fu@zerocall.com>
2012-02-08 15:37:52 +0500

    language

--- apps/agent/modules/affiliate/templates/supportingHandsetSuccess.php
+++ apps/agent/modules/affiliate/templates/supportingHandsetSuccess.php
@@ -6,202 +6,202 @@
             <tr> 
               <th><?php echo __('Brand name')?></th>
               <th><?php echo __('Model')?></th>
-              <th><?php echo __('Callback Test result')?></th>
+<!--              <th><?php echo __('Callback Test result')?></th>-->
               <th><?php echo __('Auto reboot')?></th>
               <th><?php echo __('Dialer Mode')?></th>
               <th><?php echo __('Tested by')?></th>
               <th><?php echo __('Comments')?></th>
-              <th><?php echo __('Zapna out mode')?></th>
+<!--              <th><?php echo __('Zapna out mode')?></th>-->
             </tr>
             <tr> 
               <td>&nbsp;Apple</td>
               <td>&nbsp;Iphone</td>
-              <td>&nbsp;Supported</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
               <td>&nbsp;Auto Reboot</td>
               <td>&nbsp;Mode 2</td>
               <td>&nbsp;OKH</td>
               <td>&nbsp;</td>
-              <td>&nbsp;1</td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;HTC</td>
               <td>&nbsp;Desire</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 2</td>
               <td>&nbsp;OKH</td>
               <td>&nbsp;</td>
-              <td>&nbsp;3</td>
+<!--              <td>&nbsp;3</td>-->
             </tr>
             <tr> 
               <td>&nbsp;HTC </td>
               <td>&nbsp;Touch diamond 2</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 2</td>
               <td>&nbsp;OKH</td>
               <td>&nbsp;</td>
-              <td>&nbsp;3</td>
+<!--              <td>&nbsp;3</td>-->
             </tr>
             <tr> 
               <td>&nbsp;HTC </td>
               <td>&nbsp;Touch</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 2</td>
               <td>&nbsp;OKH</td>
               <td>&nbsp;</td>
-              <td>&nbsp;1</td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;E65</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;6233</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;E71</td>
-              <td>&nbsp;Not Supported</td>
-              <td>&nbsp;Not Supported</td>
-              <td>&nbsp;Not Supported</td>
+<!--              <td>&nbsp;<?php echo __('Not Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Not Supported')?></td>
+              <td>&nbsp;<?php echo __('Not Supported')?></td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;The phone is not supported</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('The phone is not supported')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;E51</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;E65</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;N73</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;Supported</td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;N93</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;Supported</td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;6021</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 2</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;6500</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;5300 Express</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;Nokia</td>
               <td>&nbsp;1680</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Manual</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Manual')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;SE</td>
               <td>&nbsp;C902</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;SE</td>
               <td>&nbsp;M600i</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
               <td>&nbsp;</td>
-              <td>&nbsp;1</td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
             <tr> 
               <td>&nbsp;SE</td>
               <td>&nbsp;W580i</td>
-              <td>&nbsp;Not Supported</td>
-              <td>&nbsp;Not Supported</td>
-              <td>&nbsp;Not Supported</td>
+<!--              <td>&nbsp;<?php echo __('Not Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Not Supported')?></td>
+              <td>&nbsp;<?php echo __('Not Supported')?></td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Not supported</td>
+              <td>&nbsp;<?php echo __('Not Supported')?></td>
               <td>&nbsp;1</td>
             </tr>
             <tr> 
               <td>&nbsp;SE</td>
               <td>&nbsp;W750i</td>
-              <td>&nbsp;Supported</td>
-              <td>&nbsp;Auto Reboot</td>
+<!--              <td>&nbsp;<?php echo __('Supported')?></td>-->
+              <td>&nbsp;<?php echo __('Auto Reboot')?></td>
               <td>&nbsp;Mode 1</td>
               <td>&nbsp;OKH</td>
-              <td>&nbsp;Deactivation take around 4-5 min before its activated</td>
-              <td>&nbsp;1</td>
+              <td>&nbsp;<?php echo __('Deactivation take around 4-5 min before its activated')?></td>
+<!--              <td>&nbsp;1</td>-->
             </tr>
           </tbody>
         </table>
