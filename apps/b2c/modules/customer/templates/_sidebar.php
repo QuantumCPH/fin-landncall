c1900bf62b7d0715729db7bdc6f57d0542318cba
Rubab Rafique <rr@zerocall.com>
2012-02-09 14:55:43 +0500

    sidebar css fixed

--- apps/b2c/modules/customer/templates/_sidebar.php
+++ apps/b2c/modules/customer/templates/_sidebar.php
@@ -19,7 +19,6 @@
 
 	<div class="right-col">
 		<div class="box box-0">
-                    <h3></h3>
                     <p> <span style="margin-top:5px;font-weight:bold;text-align:left;color:#000;padding-top:9px;font-size:14px;"><?php echo __('Web SMS') ?></span></p><p style="color:#000;">
 <?php echo __('Send SMS worldwide at the best prices of market') ?></p>
                     <a title="<?php echo __('Web SMS') ?>" class="sidebar_button" href="<?php echo url_for('customer/websms', true) ?>"><?php echo __('Send SMS') ?></a>
@@ -29,7 +28,6 @@
 
 	<div class="right-col">
 		<div class="box box-0">
-                    <h3></h3>
                     <p> <span style="margin-top:5px;font-weight:bold;text-align:left;color:#000;padding-top:9px;font-size:14px;white-space:nowrap;"><?php echo __('Tell a friend') ?></span></p><p style="color:#000;">
 <?php echo __('International calls from 0 cents * With WLS, you can call the whole world.') ?></p>
                     <a title="<?php echo __('Tell a friend') ?>" class="sidebar_button" rel="bookmark" href="<?php echo url_for('customer/tellAFriend', true) ?>"><?php echo __('Send Invitation') ?></a>
