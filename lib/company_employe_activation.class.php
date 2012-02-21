c0faf61cb1753df3b979e41764b3c9c7e1ae60c4
Fatima Umer <fu@zerocall.com>
2012-02-21 14:09:00 +0500

    employee

--- lib/company_employe_activation.class.php
+++ lib/company_employe_activation.class.php
@@ -168,7 +168,7 @@ class CompanyEmployeActivation {
                             'password' => 'asdf1asd',
                             'h323_password' => 'asdf1asd',
                             'activation_date' => date('Y-m-d'),
-                            'batch_name' => $company->getUniqueid(),
+                            'batch_name' => $company->getVatNo(),
                             'follow_me_enabled' => $followMeEnabled
                             )));
         } catch (SoapFault $e) {
